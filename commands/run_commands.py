2b79b881985ffb7e572eeee75fa992a669a915f2
Valentyn Khoroshchak <smartgtsua@gmail.com>
2019-10-31 14:23:27 +0200

    send_requests&run_commands refactor

--- commands/run_commands.py
+++ commands/run_commands.py
@@ -8,6 +8,12 @@ from communication import send_requests
 with open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json')) as data_nodes_file:
     data_nodes_data_json = json.load(data_nodes_file)
 
+with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as files_info_file:
+    files_info_file_json = json.load(files_info_file)
+    
+with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as file:
+    file_info = json.loads(file.read())
+
 
 # TODO: check if **kwargs may be used in recognize_command()
 # TODO: wrap global vars in class
@@ -27,16 +33,16 @@ class Command:
     @staticmethod
     def make_file(content):
         Command.json_data_obj = content['make_file']
-        send_requests.make_file(Command.json_data_obj["destination_file"])
+        send_requests.make_file(Command.json_data_obj['destination_file'])
         file_info = {
-            "files": [
+            'files': [
                 {
 
-                    "file_name": Command.json_data_obj["destination_file"],
-                    "lock": False,
-                    "last_fragment_block_size": 1024,
-                    "key_ranges": None,
-                    "file_fragments": []
+                    'file_name': Command.json_data_obj['destination_file'],
+                    'lock': False,
+                    'last_fragment_block_size': 1024,
+                    'key_ranges': None,
+                    'file_fragments': []
                 }
             ]
         }
@@ -44,11 +50,11 @@ class Command:
         # file_info['files'] = []
         # file_info['files'].append(
         #     {
-        #         "file_name": json_data_obj["destination_file"],
-        #         "lock": False,
-        #         "last_fragment_block_size": 1024,
-        #         "key_ranges": None,
-        #         "file_fragments": []
+        #         'file_name': json_data_obj['destination_file'],
+        #         'lock': False,
+        #         'last_fragment_block_size': 1024,
+        #         'key_ranges': None,
+        #         'file_fragments': []
         #     }
         # )
         with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json'), 'w+') as file:
@@ -69,40 +75,36 @@ class Command:
     def append(content):
         Command.json_data_obj = content['append']
         file_name = Command.json_data_obj['file_name']
-        Command.json_data_obj = {}
-        with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as files_info_file:
-            files_info_file_json = json.load(files_info_file)
+        Command.json_data_obj = {}   
+        for item in files_info_file_json['files']:
+            if item['file_name'] == file_name:
+                if not item['file_fragments']:
+                    Command.json_data_obj['data_node_ip'] = 'http://' + data_nodes_data_json['data_nodes'][0][
+                        'data_node_address']
+                else:
+                    id = 1
 
-            for item in files_info_file_json['files']:
-                if item['file_name'] == file_name:
-                    if not item['file_fragments']:
-                        Command.json_data_obj['data_node_ip'] = 'http://' + data_nodes_data_json['data_nodes'][0][
-                            'data_node_address']
-                    else:
-                        id = 1
+                    for key, value in (item['file_fragments'][-1]).items():
+                        id = key
 
-                        for key, value in (item['file_fragments'][-1]).items():
-                            id = key
-
-                        for i in data_nodes_data_json['data_nodes']:
-                            if i['data_node_id'] == int(id):
-                                prev_ind = data_nodes_data_json['data_nodes'].index(i)
-                                if prev_ind + 1 == len(data_nodes_data_json['data_nodes']):
-                                    Command.json_data_obj['data_node_ip'] = 'http://' + \
-                                                                            data_nodes_data_json['data_nodes'][0][
-                                                                                'data_node_address']
-                                else:
-                                    Command.json_data_obj['data_node_ip'] = 'http://' + \
-                                                                            data_nodes_data_json['data_nodes'][
-                                                                                prev_ind + 1][
-                                                                                'data_node_address']
+                    for i in data_nodes_data_json['data_nodes']:
+                        if i['data_node_id'] == int(id):
+                            prev_ind = data_nodes_data_json['data_nodes'].index(i)
+                            if prev_ind + 1 == len(data_nodes_data_json['data_nodes']):
+                                Command.json_data_obj['data_node_ip'] = 'http://' + \
+                                                                        data_nodes_data_json['data_nodes'][0][
+                                                                            'data_node_address']
+                            else:
+                                Command.json_data_obj['data_node_ip'] = 'http://' + \
+                                                                        data_nodes_data_json['data_nodes'][
+                                                                            prev_ind + 1][
+                                                                            'data_node_address']
 
     # TODO: check and refactor
     @staticmethod
     def refresh_table(content):
         Command.json_data_obj = content['refresh_table']
-        with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as file:
-            file_info = json.loads(file.read())
+        
 
         for item in file_info['files']:
             if item['file_name'] == Command.json_data_obj['file_name']:
@@ -153,23 +155,22 @@ class Command:
                 })
                 mid_hash += step
 
-            with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as files_info_file:
-                files_info_file_json = json.load(files_info_file)
+            
 
-                for i in files_info_file_json['files']:
-                    arr = Command.json_data_obj['file_name'].split('.')
-                    file_name = arr[0].split('_')[0] + '.' + arr[-1]
-                    if file_name == i['file_name'].split(os.sep)[-1]:
-                        print(context['shuffle']['nodes_keys'])
-                        i['key_ranges'] = context['shuffle']['nodes_keys']
+            for i in files_info_file_json['files']:
+                arr = Command.json_data_obj['file_name'].split('.')
+                file_name = arr[0].split('_')[0] + '.' + arr[-1]
+                if file_name == i['file_name'].split(os.sep)[-1]:
+                    print(context['shuffle']['nodes_keys'])
+                    i['key_ranges'] = context['shuffle']['nodes_keys']
 
-                with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json'), 'w')as file:
-                    json.dump(files_info_file_json, file, indent=4)
+            with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json'), 'w')as file:
+                json.dump(files_info_file_json, file, indent=4)
 
-                for i in data_nodes_data_json['data_nodes']:
-                    url = 'http://' + i["data_node_address"]
-                    response = requests.post(url, data=json.dumps(context))
-                counter = 0
+            for i in data_nodes_data_json['data_nodes']:
+                url = 'http://' + i['data_node_address']
+                response = requests.post(url, data=json.dumps(context))
+            SomeClass.counter = 0
 
     @staticmethod
     def get_file(content):
@@ -177,7 +178,7 @@ class Command:
         file_name = Command.json_data_obj['file_name']
         context = {'data_nodes_ip': []}
         for i in data_nodes_data_json['data_nodes']:
-            url = 'http://' + i["data_node_address"]
+            url = 'http://' + i['data_node_address']
             context['data_nodes_ip'].append(url)
         Command.json_data_obj = context
 
@@ -187,17 +188,16 @@ class Command:
         key = Command.json_data_obj['key']
         file_name = Command.json_data_obj['file_name']
         context = {}
-        with open(os.path.join(os.path.dirname(__file__), '..', 'data', 'files_info.json')) as files_info_file:
-            files_info_file_json = json.load(files_info_file)
-            Command.json_data_obj.clear()
+        
+        Command.json_data_obj.clear()
 
-            for item in files_info_file_json['files']:
-                if item['file_name'] == file_name:
-                    Command.json_data_obj["key_ranges"] = item["key_ranges"]
-            url = 'http://' + data_nodes_data_json['data_nodes'][0]["data_node_address"]
-            context['get_hash_of_key'] = key
-            response = requests.post(url, data=json.dumps(context))
-            Command.json_data_obj['hash_key'] = response.json()
+        for item in files_info_file_json['files']:
+            if item['file_name'] == file_name:
+                Command.json_data_obj['key_ranges'] = item['key_ranges']
+        url = 'http://' + data_nodes_data_json['data_nodes'][0]['data_node_address']
+        context['get_hash_of_key'] = key
+        response = requests.post(url, data=json.dumps(context))
+        Command.json_data_obj['hash_key'] = response.json()
 
     @staticmethod
     def recognize_command(content):
--- communication/send_requests.py
+++ communication/send_requests.py
@@ -4,86 +4,58 @@ import json
 import os.path
 import requests
 
+with open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json')) as json_data:
+    data = json.load(json_data)
+
 
 def make_file(file_name):
-    json_data = open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json'))
-    data = json.load(json_data)
-    json_data.close()
-    diction = dict()
-    diction["make_file"] = {
-        "file_name": file_name
-    }
-    for item in data["data_nodes"]:
-        url = 'http://' + item["data_node_address"]
-        response = requests.post(url, data=json.dumps(diction))
-        response.raise_for_status()
-    return response.json()
+    diction = {'make_file': {
+        'file_name': file_name
+    }}
+
+    return send_request_to_data_nodes(diction)
 
 
 def map(json_data_obj):
-    mapper = json_data_obj["mapper"]
-    key_delimiter = json_data_obj["key_delimiter"]
-    field_delimiter = json_data_obj["field_delimiter"]
-    destination_file = json_data_obj["destination_file"]
     diction = {
-        "map": {
-            "mapper": mapper,
-            "field_delimiter": field_delimiter,
-            "key_delimiter": key_delimiter,
-            "destination_file": destination_file
+        'map': {
+            'mapper': json_data_obj['mapper'],
+            'field_delimiter': json_data_obj['field_delimiter'],
+            'key_delimiter': json_data_obj['key_delimiter'],
+            'destination_file': json_data_obj['destination_file']
         }
     }
     if 'server_source_file' in json_data_obj:
-        server_src = json_data_obj['server_source_file']
-        diction["map"]["server_src"] = server_src
+        diction['map']['server_src'] = json_data_obj['server_source_file']
     else:
-        src = json_data_obj['source_file']
-        diction["map"]['source_file'] = src
-    with open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json')) as json_data:
-        data = json.load(json_data)
-        for item in data["data_nodes"]:
-            url = 'http://' + item["data_node_address"]
-            response = requests.post(url, data=json.dumps(diction))
-            response.raise_for_status()
-        return response.json()
+        diction['map']['source_file'] = json_data_obj['source_file']
+
+    return send_request_to_data_nodes(diction)
 
 
 def reduce(json_data_obj):
-    print("+++")
-    print(json_data_obj)
-    print('+++')
-    key_delimiter = json_data_obj["key_delimiter"]
-    destination_file = json_data_obj["destination_file"]
-    reducer = json_data_obj['reducer']
-    with open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json')) as json_data:
-        data = json.load(json_data)
     diction = {
-        "reduce": {
-            "reducer": reducer,
-            "key_delimiter": key_delimiter,
-            "destination_file": destination_file
+        'reduce': {
+            'reducer': json_data_obj['reducer'],
+            'key_delimiter': json_data_obj['key_delimiter'],
+            'destination_file': json_data_obj['destination_file']
         }
     }
     if 'server_source_file' in json_data_obj:
-        server_src = json_data_obj['server_source_file']
-        diction["reduce"]["server_src"] = server_src
+        diction['reduce']['server_src'] = json_data_obj['server_source_file']
     else:
-        src = json_data_obj['source_file']
-        diction["reduce"]["source_file"] = src
+        diction['reduce']['source_file'] = json_data_obj['source_file']
 
-    for item in data["data_nodes"]:
-        url = 'http://' + item["data_node_address"]
-        response = requests.post(url, data=json.dumps(diction))
-        response.raise_for_status()
-    return response.json()
+    return send_request_to_data_nodes(diction)
 
 
 def clear_data(context):
-    with open(os.path.join(os.path.dirname(__file__), '..', 'config', 'json', 'data_nodes.json')) as json_data:
-        data = json.load(json_data)
+    return send_request_to_data_nodes(context)
 
-        for item in data['data_nodes']:
-            url = 'http://' + item['data_node_address']
-            response = requests.post(url, data=json.dumps(context))
-            response.raise_for_status()
-        return response.json()
+
+def send_request_to_data_nodes(context):
+    for item in data['data_nodes']:
+        url = 'http://' + item['data_node_address']
+        response = requests.post(url, data=json.dumps(context))
+        response.raise_for_status()
+    return response.json()
