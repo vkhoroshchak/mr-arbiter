8ededaa73dcf3ab3d21dd2bea3b30899ad324eba
Valentyn Khoroshchak <valentyn.khoroshchak@jetsoftpro.com>
2021-05-10 00:45:59 +0300

    Update __exit__ method in FileDBManager class

--- local_database/utils.py
+++ local_database/utils.py
@@ -11,10 +11,6 @@ class FileDBManager:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_tb is None:
-            self.session.commit()
-        else:
-            self.session.rollback()
         self.session.close()
 
     def save(self, obj):
