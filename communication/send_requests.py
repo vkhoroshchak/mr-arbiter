3d6b06df9cd94bbd5af87e1e29d4ff51c1e1ed94
mranch <anchiforov@gmail.com>
2021-11-06 01:11:09 +0200

    request is sent only to relevant nodes

--- communication/send_requests.py
+++ communication/send_requests.py
@@ -1,6 +1,5 @@
 import asyncio
 import datetime
-
 from aiohttp import ClientSession
 
 import schemas
@@ -16,7 +15,7 @@ async def create_config_and_filesystem(file_name, file_id):
                                             'create_config_and_filesystem')
 
 
-async def send_request_to_data_nodes(data_to_data_node, command):
+async def send_request_to_data_nodes(data_to_data_node, command, data_nodes=config.data_nodes):
     logger.info(f"Send request to data nodes: {data_to_data_node}")
     async with ClientSession() as session:
         async def send_request(ip_address):
@@ -33,25 +32,34 @@ async def send_request_to_data_nodes(data_to_data_node, command):
                 pass
 
         tasks = []
-        for data_node in config.data_nodes:
+        for data_node in data_nodes:
             tasks.append(asyncio.ensure_future(send_request(data_node["data_node_address"])))
 
         await asyncio.gather(*tasks)
 
 
+def get_file_data_nodes_list(file_id: str):
+    file_db_manager = FileDBManager()
+    file_in_db = file_db_manager.get(file_id)
+    return list(file_in_db["file_fragments"].keys())
+
+
 async def start_map_phase(map_request):
-    return await send_request_to_data_nodes(map_request.__dict__, 'map')
+    return await send_request_to_data_nodes(map_request.__dict__, 'map',
+                                            data_nodes=get_file_data_nodes_list(map_request.file_id))
 
 
 async def start_reduce_phase(reduce_request):
-    return await send_request_to_data_nodes(reduce_request.__dict__, 'reduce')
+    return await send_request_to_data_nodes(reduce_request.__dict__, 'reduce',
+                                            data_nodes=get_file_data_nodes_list(reduce_request.file_id))
 
 
 async def min_max_hash(shuffle_request: schemas.StartShufflePhaseRequest):
-    return await send_request_to_data_nodes(shuffle_request.__dict__, 'min_max_hash')
+    return await send_request_to_data_nodes(shuffle_request.__dict__, 'min_max_hash',
+                                            data_nodes=get_file_data_nodes_list(shuffle_request.file_id))
 
 
-async def generate_hash_ranges(hash_request: schemas.HashRequest):
+async def generate_hash_ranges(hash_request: schemas.HashRequest, data_nodes=config.data_nodes):
     logger.info("Received hash key ranges")
     logger.info(f"Min hash value: {hash_request.min_hash_value}")
     logger.info(f"Max hash value: {hash_request.max_hash_value}")
@@ -85,7 +93,7 @@ async def generate_hash_ranges(hash_request: schemas.HashRequest):
         }
         mid_hash = min_hash
 
-        for index, data_node in enumerate(config.data_nodes):
+        for index, data_node in enumerate(data_nodes):
             if index == len(data_nodes_ip_addresses):
                 end_hash = max_hash
             else:
@@ -102,50 +110,23 @@ async def generate_hash_ranges(hash_request: schemas.HashRequest):
 
         logger.info(f"Data to data nodes: {data_to_data_node}")
         logger.info("Sending data to data nodes")
-        await send_request_to_data_nodes(data_to_data_node, 'shuffle')
+        await send_request_to_data_nodes(data_to_data_node, 'shuffle',
+                                         data_nodes=get_file_data_nodes_list(hash_request.file_id))
 
 
 async def clear_data(clear_data_request: schemas.ClearDataRequest):
     shuffle_db_manager = ShuffleDBManager()
-    file_db_obj = FileDBManager().get(clear_data_request.file_id)
+    file_db_manager = FileDBManager()
+    file_db_obj = file_db_manager.get(clear_data_request.file_id)
 
     clear_data_request.folder_name = file_db_obj["file_name"]
 
     if clear_data_request.remove_all_data:
         shuffle_db_manager.delete(clear_data_request.file_id)
+        file_db_manager.delete(clear_data_request.file_id)
     else:
         shuffle_db_manager.reset_obj(clear_data_request.file_id)
+        file_db_manager.reset_obj(clear_data_request.file_id)
 
-    return await send_request_to_data_nodes(clear_data_request.__dict__, 'clear_data')
-
-
-# async def get_file(content: dict):
-    # return await send_request_to_data_nodes(content, 'get_file')
-    # return FileDBManager().get(content['file_id'])['file_name']
-    # logger.info(f"Send request to data nodes: {data_to_data_node}")
-    # async with ClientSession() as session:
-    #     async def send_request(ip_address):
-    #         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    #         url = f'http://{ip_address}/command/get_file'  # noqa
-    #         try:
-    #             file_parts.append(await session.request(url=url,
-    #                                                     headers=headers,
-    #                                                     json=content,
-    #                                                     timeout=10,
-    #                                                     method="GET"))
-    #             # res = resp.
-    #             # file_parts.append(resp)
-    #             # print(136, resp)
-    #             # return resp
-    #         except asyncio.exceptions.TimeoutError:
-    #             pass
-    #
-    #     tasks = []
-    #     file_parts = []
-    #     for data_node in config.data_nodes:
-    #         await send_request(data_node["data_node_address"])
-    #         # tasks.append(asyncio.ensure_future(send_request(data_node["data_node_address"])))
-    #
-    #     await asyncio.gather(*tasks)
-    #     print(148, file_parts)
-    #     return file_parts
+    return await send_request_to_data_nodes(clear_data_request.__dict__, 'clear_data',
+                                            data_nodes=get_file_data_nodes_list(clear_data_request.file_id))
